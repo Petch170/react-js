1ce392e364ca2abd86e3f3961d9834209fc5bb3f
Your Name <you@example.com>
2023-12-27 12:06:13 +0700

    finish

--- react-3/exercise-1/src/App.jsx
+++ react-3/exercise-1/src/App.jsx
@@ -2,39 +2,49 @@ import React, { useState } from 'react';
 
 function App() {
     // adding state here.
+  const [info, setInfo] = useState();
 
   const handleClick = (value) => {
     // code here.
+    setInfo(value);
   };
 
   return (
     <div>
-      <button>Fullname</button>
-      <button>Age</button>
-      <button>Picture</button>
-      <DisplayInfo />
+      <button onClick={()=> handleClick('Fullname')}>Fullname</button>
+      <button onClick={()=> handleClick('Age')}>Age</button>
+      <button onClick={()=>handleClick('Picture')}>Picture</button>
+      <DisplayInfo info1={info}/>
     </div>
   );
 }
+//ไม่ใช้ const handleClick ให้ใส่ในreturnเป็น <button onClick={()=> setInfo('Fullname)}> ทีละตัว
 
-function DisplayInfo(props) {
 
-  let ___;
-  if (___ === 'Fullname') {
-    ___ = <h2>John Doe</h2>;
-  } else if (___ === 'Age') {
-    ___ = <h2>30</h2>;
-  } else if (___ === 'Picture') {
-    ___ = <img src="https://via.placeholder.com/150" alt="Placeholder" />;
+function DisplayInfo({info1}) {
+  //ฟังก์ชันใส่เป็น props ก็ได้
+//ต้อง set counst {info1}=props; ด้วย
+let content; // 
+  if ( info1=== 'Fullname') { // ถ้าในfunction ใส่เป็นprops ให้ใส่ในif เป็นif(props.info1)ตามค่าที่เซตไว้ในDisplayInfo
+    content = <h2>John Doe</h2>;
+  } else if (info1 === 'Age') {
+    content = <h2>30</h2>;
+  } else if (info1 === 'Picture') {
+    content = <img src="https://via.placeholder.com/150" alt="Placeholder" />;
   } else {
-    ___ = <p>Please select an option.</p>;
+    content = <p>Please select an option.</p>;
   }
 
   return (
     <div>
-      {___}
+      {content}
     </div>
   );
 }
 
 export default App;
+
+// getname
+// 1.1)object.name
+// 1.2)declare variable  const name = object.name
+// 1.3)destuc..    const {name} = object
